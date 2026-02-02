8d6059a19af2e9f1b5e3f562a59289fbe2bd45a4
Josting Ramos <Jostinramos12@gmail.com>
2026-02-02 14:24:43 -0500

    front end

--- teamceleste-front/src/App.jsx
+++ teamceleste-front/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>cambios aqui</h1>
+      <h1>Maquina expendederora</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
