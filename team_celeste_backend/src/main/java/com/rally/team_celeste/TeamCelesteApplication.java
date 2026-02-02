2d65abe79c5b394ec6ce479995867fe2bf00b70b
Bryan Hernandez <bryan.ahs@hotmail.com>
2026-02-02 13:57:07 -0500

    Reestructurando 1

---
+++ team_celeste_backend/src/main/java/com/rally/team_celeste/TeamCelesteApplication.java
@@ -0,0 +1,13 @@
+package com.rally.team_celeste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamCelesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamCelesteApplication.class, args);
+	}
+
+}
