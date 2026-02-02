82aa3354fe196db358adeb63acabef28da08dd86
Bryan Hernandez <bryan.ahs@hotmail.com>
2026-02-02 14:13:48 -0500

    Backend

---
+++ team_celeste_backend/src/test/java/com/rally/team_celeste/TeamCelesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rally.team_celeste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamCelesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
